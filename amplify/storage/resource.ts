b97ed63d0e2e408d6b11227e6bf0e3830c7d3858
Alan Tsang <alan.tsang@vilab.io>
2025-01-22 14:40:17 +0800

    update s3 permission

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,11 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'unityhologramiotupdate'
+  name: 'unityhologramiotupdate',
+  access: (allow) => ({
+    '*': [
+      allow.guest.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+  })
 });
