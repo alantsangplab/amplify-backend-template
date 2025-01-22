cc39065b7b0dd0dee5d97686433bf18172630a07
Alan Tsang <alan.tsang@vilab.io>
2025-01-22 14:45:01 +0800

    update path

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'unityhologramiotupdate',
   access: (allow) => ({
-    '*': [
+    'example/*': [
       allow.guest.to(['read']),
       allow.entity('identity').to(['read', 'write', 'delete'])
     ],
